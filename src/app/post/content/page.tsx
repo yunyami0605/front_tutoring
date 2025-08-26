d18e6e79c80b742b8e0d169e471f4157a91d7813
yunjongyun <yunyami0605@gmail.com>
2025-08-26 14:10:40 +0900

    feat: 게시글 목록, 컨텐츠 추가
    1. ui 및 api 조회 기능 추가

--- src/app/post/content/page.tsx
+++ src/app/post/content/page.tsx
@@ -1,5 +1,70 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import type { PostItem } from '../../../features/post/_types/data';
+import axios from 'axios';
+import { useAccessTokenStore } from '../../../features/auth/_stores/accessToken.store';
+import { FaRegHeart, FaRegComment, FaRegBookmark } from 'react-icons/fa';
+import styles from './PostContentPage.module.css';
+
 function PostContentPage() {
-  return <div></div>;
+  const { id } = useParams<{ id: string }>();
+  const [data, setData] = useState<PostItem>();
+  const { accessToken } = useAccessTokenStore();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/post/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken} `,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setData(response.data);
+        }
+      });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {/* 상단 프로필 영역 */}
+      <div className={styles.profile}>
+        <img
+          src="/images/image_tmp.jpg"
+          alt="profile"
+          className={styles.profileImage}
+        />
+        <strong>ldh_sky</strong>
+      </div>
+
+      {/* 게시글 이미지 */}
+      <div>
+        <img
+          src="/images/image_tmp.jpg"
+          alt="post"
+          className={styles.postImage}
+        />
+      </div>
+
+      {/* 버튼 영역 */}
+      <div className={styles.actions}>
+        <div className={styles.actionsLeft}>
+          <FaRegHeart />
+          <FaRegComment />
+        </div>
+        <div className={styles.actionsRight}>
+          <FaRegBookmark />
+        </div>
+      </div>
+
+      {/* 본문 영역 */}
+      <div className={styles.content_wrapper}>
+        <p className={styles.title}>{data?.title ?? ''}</p>
+
+        <p className={styles.content}>{data?.content ?? ''}</p>
+      </div>
+    </div>
+  );
 }
 
 export default PostContentPage;
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,5 +1,44 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAccessTokenStore } from '../../features/auth/_stores/accessToken.store';
+import type { GetPostsResponse } from '../../features/post/_types/response';
+import type { PostItem as PostItemType } from '../../features/post/_types/data';
+import PostItem from '../../features/post/_components/PostItem';
+import { useNavigate } from 'react-router-dom';
+
 function PostsPage() {
-  return <div></div>;
+  const { accessToken } = useAccessTokenStore();
+  const [posts, setPosts] = useState<PostItemType[]>([]);
+
+  const navigate = useNavigate();
+
+  const onNavigate = (id: string) => {
+    navigate(`/post/${id}`);
+  };
+
+  useEffect(() => {
+    axios
+      .get<GetPostsResponse>('http://localhost:4000/post', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        setPosts(response.data);
+
+        return response;
+      });
+  }, []);
+
+  return (
+    <div>
+      {posts.map((item) => (
+        <React.Fragment key={item.id}>
+          <PostItem onClick={() => onNavigate(item.id)} {...item} />
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
 
 export default PostsPage;
